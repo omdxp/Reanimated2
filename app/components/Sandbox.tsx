5052c8c3d7116743f2efde2b4798b83a5fd28a10
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-05-08 23:52:10 +0100

    Implementing worklets.

--- app/components/Sandbox.tsx
+++ app/components/Sandbox.tsx
@@ -1,17 +1,39 @@
-
 // import react native
 import React, {FC} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button, StyleSheet} from 'react-native';
+import {runOnUI} from 'react-native-reanimated';
 
 // define Sandbox props interface
 interface SandboxProps {}
 
 // export Sandbox component
 const Sandbox: FC<SandboxProps> = ({}): JSX.Element => {
+  function someWorklet(greeting: string) {
+    'worklet';
+    console.log(greeting, 'From the UI thread');
+  }
+  function onPress() {
+    runOnUI(someWorklet)('Howdy');
+  }
   return (
-    <View>
-      <Text>Sandbox component created!</Text>
+    <View style={styles.mainView}>
+      <View style={styles.centerView}>
+        <Button onPress={onPress} title={'Run On UI'} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    padding: 9,
+  },
+  centerView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 export default Sandbox;
